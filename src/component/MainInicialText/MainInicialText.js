7f16e427bad9ebb607e5291068ade8ddecbd6692
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-07-22 18:25:20 -0300

    Feat: changed some infos

--- src/component/MainInicialText/MainInicialText.js
+++ src/component/MainInicialText/MainInicialText.js
@@ -7,7 +7,7 @@ export default function MainInicialText() {
         <S.InicialTextDiv>
             <S.TitleBox>
                 <S.Title>
-                    Os Objetivos de Desenvolvimento Sustentável no Brasil
+                    Os Objetivos de Desenvolvimento Sustentável no Meu Bairro
                 </S.Title>
             </S.TitleBox>
 
@@ -16,7 +16,7 @@ export default function MainInicialText() {
                     Os Objetivos de Desenvolvimento Sustentável são um apelo global à ação para acabar com a pobreza,
                      proteger o meio ambiente e o clima e garantir que as pessoas, em todos os lugares, possam desfrutar de 
                      paz e de prosperidade. Estes são os objetivos para os quais as Nações Unidas estão contribuindo a fim de 
-                     que possamos atingir a Agenda 2030 no Brasil.
+                     que possamos atingir a Agenda 2030 no Bairro.
                 </S.P>
             </S.TextDiv>
         </S.InicialTextDiv>
